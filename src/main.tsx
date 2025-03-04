492926a3d59a3793f3244e4476a1bfcd70422baa
Thiago Loureiro <thiagoguaru@me.com>
2025-03-03 22:55:10 -0500

    Add Sentry integration for error tracking and performance monitoring

--- src/main.tsx
+++ src/main.tsx
@@ -5,6 +5,20 @@ import { MsalProvider } from "@azure/msal-react";
 import { msalConfig } from './auth/msalConfig';
 import App from './App';
 import './index.css';
+import * as Sentry from "@sentry/react";
+
+Sentry.init({
+  dsn: import.meta.env.VITE_APP_SENTRY_DSN,
+  integrations: [
+    Sentry.browserTracingIntegration(),
+    Sentry.replayIntegration(),
+  ],
+  tracesSampleRate: 1.0,
+  tracePropagationTargets: ["localhost", /^https:\/\/yourserver\.io\/api/],
+  replaysSessionSampleRate: 0.1,
+  replaysOnErrorSampleRate: 1.0,
+});
+
 
 const msalInstance = new PublicClientApplication(msalConfig);
 
